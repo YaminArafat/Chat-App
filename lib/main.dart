e056bebe31ebce9370df6ce5bda527062bdd8773
YaminArafat <yaminarafat032@gmail.com>
2021-11-26 05:13:52 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(),
+      home: null,
+    );
+  }
+}
